b36cb16cf77f5607ef5bd4ee99a75c4f7da278e6
MastaWu <GreatShamanGT@gmail.com>
2016-07-29 20:02:05 -0400

    Fixed sql statements.

--- resources/database/create database.sql
+++ resources/database/create database.sql
@@ -1,6 +1,6 @@
-/*CREATE DATABASE gnom_app;*/
+CREATE DATABASE IF NOT EXISTS gnom_app;
 
-CREATE TABLE restaurant (
+CREATE TABLE IF NOT EXISTS restaurant (
 	restaurant_id INT NOT NULL AUTO_INCREMENT,
     restaurant_name VARCHAR(50) NOT NULL,
     address VARCHAR(50) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE restaurant (
     PRIMARY KEY (restaurant_id)
     );
     
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
 	user_id INT NOT NULL AUTO_INCREMENT,
 	user_name VARCHAR(50) NOT NULL,
 	user_password VARCHAR(50) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE user (
 	PRIMARY KEY (user_id)
 	);
     
-CREATE TABLE restaurant_admin (
+CREATE TABLE IF NOT EXISTS restaurant_admin (
 	restaurant_admin_id INT NOT NULL AUTO_INCREMENT,
     restaurant_id INT NOT NULL,
     user_id INT NOT NULL,
@@ -29,7 +29,18 @@ CREATE TABLE restaurant_admin (
     FOREIGN KEY (user_id) REFERENCES user(user_id)
     );
     
-CREATE TABLE deal (
+CREATE TABLE IF NOT EXISTS restaurant_review (
+	restaurant_review_id INT NOT NULL AUTO_INCREMENT,
+	restaurant_id INT NOT NULL,
+	restaurant_review VARCHAR(200) NOT NULL,
+	restaurant_photo VARCHAR(200) NULL,
+	restaurant_rating INT NULL,
+	user_id INT NOT NULL,
+	PRIMARY KEY (restaurant_review_id),
+	FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+    
+CREATE TABLE IF NOT EXISTS deal (
 	deal_id INT NOT NULL AUTO_INCREMENT,
     restaurant_id INT NOT NULL,
     meal_type VARCHAR(50) NOT NULL,
@@ -38,22 +49,21 @@ CREATE TABLE deal (
     date_end DATE NOT NULL,
     progress varchar(30),
     price float NOT NULL,
-    restaurant_id INT NOT NULL,
     PRIMARY KEY (deal_id),
     FOREIGN KEY (restaurant_id) REFERENCES restaurant(restaurant_id)
 	);
     
-CREATE TABLE deal_comment (
-	deal_comment_id INT NOT NULL AUTO_INCREMENT,
-    deal_comment VARCHAR(200) NOT NULL,
+CREATE TABLE IF NOT EXISTS deal_review (
+	deal_review_id INT NOT NULL AUTO_INCREMENT,
+    deal_review VARCHAR(200) NOT NULL,
     deal_photo VARCHAR(200) NOT NULL,
     deal_rating INT NULL,
     user_id INT NOT NULL,
-    PRIMARY KEY (deal_comment_id),
+    PRIMARY KEY (deal_review_id),
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );    
     
-CREATE TABLE invoice (
+CREATE TABLE IF NOT EXISTS invoice (
 	invoice_id INT NOT NULL AUTO_INCREMENT,
     deal_id INT NOT NULL,
     user_id INT NOT NULL,
